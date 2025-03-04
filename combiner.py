bc5bcc31554296d532db1149cf90777f34df8862
Kirera Wainaina <richardwainainak@gmail.com>
2025-03-04 11:56:44 +0000

    Input and output in the same format

--- combiner.py
+++ combiner.py
@@ -11,22 +11,22 @@ rating_count = 0
 
 for line in sys.stdin:
     try:
-        key, rating = line.strip().split('\t')
-        rating = Decimal(rating)
+        key, rating_sum_str, rating_count_str = line.strip().split('\t')
+        rating_sum_input = Decimal(rating_sum_str)
+        rating_count_input = int(rating_count_str)
 
         if current_key != key:
             if current_key is not None:
                 print(f"{current_key}\t{rating_sum}\t{rating_count}")
             current_key = key
-            rating_sum = rating
-            rating_count = 1
+            rating_sum = rating_sum_input
+            rating_count = rating_count_input
         else:
-            rating_sum += rating
-            rating_count += 1
+            rating_sum += rating_sum_input
+            rating_count += rating_count_input
 
     except Exception as e:
         print(f"Error in line: {line.strip()} - {str(e)}", file=sys.stderr)
 
-# Output final group
 if current_key is not None:
     print(f"{current_key}\t{rating_sum}\t{rating_count}")
