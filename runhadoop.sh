6d7783ee67d1a43d1efa095c699df878eacc72c5
Kirera Wainaina <richardwainainak@gmail.com>
2025-03-04 11:35:46 +0000

    Add combiner to runhadoop file

--- runhadoop.sh
+++ runhadoop.sh
@@ -6,10 +6,11 @@ rm -rf results/
 
 # run hadoop command
 hadoop jar /home/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.6.jar \
--D mapred.reduce.tasks=1 \
--files mapper.py,reducer.py,years.txt,genres.txt \
+-D mapred.reduce.tasks=2 \
+-files mapper.py,reducer.py,combiner.py,years.txt,genres.txt \
 -mapper mapper.py \
 -reducer reducer.py \
+-combiner combiner.py \
 -input /user/rkw00013/assignment-docs/ratings.txt \
 -output /user/rkw00013/assignment-docs/results
 
